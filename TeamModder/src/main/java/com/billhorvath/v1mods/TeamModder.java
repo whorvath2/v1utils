cad35053012a6bf7ee2450ac8a764af6482a526d
William Horvath II <bill@billh.local>
2015-06-19 17:07:28 -0400

    Successfully querying for Members.

--- TeamModder/src/main/java/com/billhorvath/v1mods/TeamModder.java
+++ TeamModder/src/main/java/com/billhorvath/v1mods/TeamModder.java
@@ -2,14 +2,18 @@ package com.billhorvath.v1mods;
 
 import com.versionone.*;
 import com.versionone.apiclient.*;
+import com.versionone.apiclient.services.*;
+import com.versionone.apiclient.interfaces.*;
+
 
 /**
 */
 
 public class TeamModder{
-	private static V1Connector connector;
+	private final Services services;
 
 	public TeamModder(){
+		V1Connector connector = null;
 		try{
 			connector = V1Connector
 				.withInstanceUrl("https://www8.v1host.com/ParishSOFTLLC/")
@@ -20,13 +24,18 @@ public class TeamModder{
 		catch (Exception e){
 			e.printStackTrace();
 		}
+		assert connector != null;
+		
+		this.services = new Services(connector);
+		
 	}
 	
 	/*
 	*/
 	public static void main(String[] arguments){
 		TeamModder modder = getInstance();
-		System.out.println(modder);
+		System.out.println(modder.memberList());
+		
 	}
 	/*
 	*/
@@ -34,9 +43,33 @@ public class TeamModder{
 		return new TeamModder();
 	}
 	/*
+	Returns a String representation of the members attached to this VersionOne installation.
+	*/
+	public String memberList(){
+		IAssetType assetType = services.getMeta().getAssetType("Member");
+		Query query = new Query(assetType);
+		IAttributeDefinition nameAttr = assetType.getAttributeDefinition("Name");
+		query.getSelection().add(nameAttr);
+		String str = "VersionOne Members:"; 
+		try{
+			QueryResult result = services.retrieve(query);
+		
+			for (Asset member: result.getAssets()){
+				str += "\n\tNumber: " + member.getOid().getToken() 
+					+ "\tName: " + member.getAttribute(nameAttr).getValue();
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			str += " ERROR QUERYING FOR MEMBERS";
+		}
+		return str;
+	}
+	
+	/*
 	*/
 	@Override
 	public String toString(){
-		return "TeamModder:\n\tConnector: " + connector;
+		return "TeamModder:\n\tservices: " + services;
 	}
 }
