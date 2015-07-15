2658307b618049be73266b8c79c49e6619a10056
Bill Horvath <bhorvath@parishsoft.com>
2015-07-15 17:21:35 -0400

    Adding package-level documentation.

---
+++ TeamModder/src/main/java/com/billhorvath/v1mods/package-info.java
@@ -0,0 +1,10 @@
+/*
+v1mods is a collection of utilities and objects designed for use with the VersionOne API.
+
+<h2>Usage Notes</h2>
+<p>This package stores the access token for the VersionOne API in encrypted form in a plain-text file called <span style="font-family: courier;">enc_token</span>. To create a new encrypted access token, use the Token Utilities from the command line to convert the token from plain text: <div>java -jar [jarFileName] com.billhorvath.v1mods.TokenUtils [unencryptedToken]</div></p>
+<p>Note that the key for encrypting and decrypting the token is stored in the <span style="font-family: courier;">key</span> file. Neither enc_token nor key are included in the source code repository for security reasons; see Bill to get a copy of the current key and enc_token files, or a VersionOne admin to get new access tokens created.</p>
+
+</p>
+*/
+package com.billhorvath.v1mods;
