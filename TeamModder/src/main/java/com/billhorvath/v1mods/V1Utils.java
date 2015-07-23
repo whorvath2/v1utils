f29b6712c2f0e521cd8ed988b71aee8548bd62a2
Bill Horvath <bhorvath@parishsoft.com>
2015-07-23 19:53:14 -0400

    Added some convenience methods and refactored.

--- TeamModder/src/main/java/com/billhorvath/v1mods/V1Utils.java
+++ TeamModder/src/main/java/com/billhorvath/v1mods/V1Utils.java
@@ -3,6 +3,7 @@ package com.billhorvath.v1mods;
 import java.util.*;
 import com.versionone.*;
 import com.versionone.apiclient.*;
+import com.versionone.apiclient.filters.*;
 import com.versionone.apiclient.services.*;
 import com.versionone.apiclient.interfaces.*;
 
@@ -22,16 +23,49 @@ class V1Utils{
 	Returns a List of Assets of the assetTypeStr flavor and with attributeNames attributes attached.
 	*/
 	static List<Asset> findAssets(String assetTypeStr, String...attributeNames){
+		Query query = buildQuery(assetTypeStr);
+		return findAssets(query, assetTypeStr, attributeNames);
+	}
+	/**
+	Returns a List of Assets of the assetTypeStr flavor and with attributeNames attributes attached.
+	*/
+	static List<Asset> findAssets(String assetTypeStr, IFilterTerm term, String...attributeNames){
+	
+		Query query = buildQuery(assetTypeStr);
+		query.setFilter(term);		
+		return findAssets(query, assetTypeStr, attributeNames);
+	}
+	/**/
+	static Query buildQuery(String assetTypeStr){
+		IServices services = V1Services.getInstance().services();		
+		IAssetType assetType = assetType(assetTypeStr);
+		Query query = new Query(assetType);
+		return query;
+	}
+	/**/
+	static IAssetType assetType(String assetTypeStr){
+		IServices services = V1Services.getInstance().services();
+		return services.getMeta().getAssetType(assetTypeStr);
+	}
+	/**
+	*/
+	static IAttributeDefinition getAttribute(String assetTypeStr, 
+		String attrName){
+
+			IAttributeDefinition attributeDef = assetType(assetTypeStr)
+					.getAttributeDefinition(attrName);
+			return attributeDef;
+	}
+	/**
+	*/
+	static List<Asset> findAssets(Query query, String assetTypeStr, String...attributeNames){
 
 		IServices services = V1Services.getInstance().services();
-		List<Asset> result = null;
+		List<Asset> result = new ArrayList<Asset>();
 		
-		IAssetType assetType = services.getMeta().getAssetType(assetTypeStr);
-		Query query = new Query(assetType);
-
 		for (String attributeName: attributeNames){
 			IAttributeDefinition attribute = 
-				assetType.getAttributeDefinition(attributeName);
+				getAttribute(assetTypeStr, attributeName);
 			query.getSelection().add(attribute);
 		}
 		try{
@@ -43,17 +77,26 @@ class V1Utils{
 			assert false;
 		}
 		return result;
+
 	}
+	
 	/**
 	*/
-	static IAttributeDefinition getAttribute(String assetTypeStr, 
-		String attrName){
-
+	static Asset findAssetByOid(String oidStr){
+			Asset result = null;
 			IServices services = V1Services.getInstance().services();
-			IAttributeDefinition attributeDef =
-				services.getMeta().getAssetType(assetTypeStr)
-					.getAttributeDefinition(attrName);
-			return attributeDef;
+			try{
+				Oid oid = services.getOid(oidStr);
+				Query query = new Query(oid);
+				QueryResult queryResult = services.retrieve(query);
+				assert queryResult.getAssets().length == 1;
+				result = queryResult.getAssets()[0];
+			}
+			catch(Exception e){
+				e.printStackTrace();
+				assert false;
+			}
+			return result;
 	}
 }
 
